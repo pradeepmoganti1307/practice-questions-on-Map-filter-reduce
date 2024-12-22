9b4455c40d8b5a42023500731e871a55db02a388
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2024-12-22 17:59:22 +0530

    added filterLongWords()

--- 01_10_filter.js
+++ 01_10_filter.js
@@ -9,3 +9,21 @@ const filterEvenNumbers = function (numbers) {
 
 console.log('01.even numbers [1, 2, 3, 4, 5] => [2, 4], output :', filterEvenNumbers([1, 2, 3, 4, 5]));
 console.log('02.even numbers [1, 5] => [], output :', filterEvenNumbers([1, 5]));
+
+// words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
+const isGreaterThan = function (threshold) {
+  return function (number) {
+    return number > threshold;
+  };
+};
+
+const isWordLongerThan = function (word) {
+  return isGreaterThan(5)(word.length);
+};
+
+const filterLongWords = function (words) {
+  return words.filter(isWordLongerThan);
+};
+
+console.log('\n03.words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"], output: ', filterLongWords(["apple", "banana", "kiwi", "grape"]));
+console.log('04.words with more than 5 letters ["apples", "banana", "oranges", "grapes"] => ["banana"], output:', filterLongWords(["apples", "banana", "oranges", "grapes"]));
