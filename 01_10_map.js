86ceb98c6b7447e992cb29a742dc76b3355dd97c
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2024-12-22 13:12:24 +0530

    added lengthOf function

--- 01_10_map.js
+++ 01_10_map.js
@@ -10,3 +10,14 @@ const squaresOf = function (numbers) {
 console.log("1.squares of [1, 2, 3] is [1, 4, 9], output:", squaresOf([1, 2, 3]));
 console.log("2.squares of [] is [], output:", squaresOf([]));
 
+// lengths of ["apple", "banana", "kiwi"] => [5, 6, 4]
+const lengthOf = function (string) {
+  return string.length;
+}
+
+const lengthsOf = function (strings) {
+  return strings.map(lengthOf);
+};
+
+console.log('\n3.lengths of ["apple", "banana", "kiwi"] => [5, 6, 4], output:', lengthsOf(["apple", "banana", "kiwi"]));
+console.log('4.lengths of ["a", "b", "k"] => [1, 1, 1], output:', lengthsOf(["a", "b", "k"]));
