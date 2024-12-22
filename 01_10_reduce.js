c5e85c592a301e3b5d958313fa2cecdeadc16aa2
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2024-12-22 22:08:59 +0530

    added productOf()

--- 01_10_reduce.js
+++ 01_10_reduce.js
@@ -5,3 +5,11 @@ const sumOf = function (numbers) {
 
 console.log('\n01.sumOf([1, 2, 3, 4]) => 10, output:', sumOf([1, 2, 3, 4]));
 console.log('02.sumOf([]) => 0, output:', sumOf([]));
+
+// productOf([1, 2, 3, 4]) => 24
+const productOf = function (numbers) {
+  return numbers.reduce((product, number) => product * number, 1);
+};
+
+console.log('\n03.productOf([1, 2, 3, 4]) => 24, output:', productOf([1, 2, 3, 4]));
+console.log('\n03.productOf([]) => 1, output:', productOf([]));
